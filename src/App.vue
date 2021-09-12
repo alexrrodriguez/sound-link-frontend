ede1944c258444fd174a22a4c52ff356853b3895
alex <arodriguez202020@gmail.com>
2021-09-12 11:05:48 -0500

    create login, logout, and signup pages, add authentication to main.js and routing to index.js files

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,12 @@
       <router-link to="/charts">Top 50 Charts</router-link>
       |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/login">Login</router-link>
+      |
+      <router-link to="/logout">Logout</router-link>
+      |
+      <router-link to="/signup">Signup</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Charts from "../views/Charts.vue";
 import Artists from "../views/Artists.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
@@ -29,6 +32,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
   },
 ];
 
