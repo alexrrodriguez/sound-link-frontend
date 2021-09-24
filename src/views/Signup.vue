a245abf037cda47cf73a123207e41f367db834d1
alex <arodriguez202020@gmail.com>
2021-09-24 13:30:58 -0500

    update api request url on signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -73,7 +73,7 @@ export default {
   methods: {
     submit: function () {
       axios
-        .post("http://localhost:3000/users", this.newUserParams)
+        .post("/users", this.newUserParams)
         .then((response) => {
           console.log(response.data);
           this.$router.push("/login");
