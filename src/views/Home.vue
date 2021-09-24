0eb39cad9d89680240b8547358fa2f68972f5534
alex <arodriguez202020@gmail.com>
2021-09-24 14:27:39 -0500

    add back base url to api call on home page, change axios.defaults base url on main.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -346,7 +346,9 @@ export default {
     randomTicket: function () {
       let apiKey = process.env.VUE_APP_TICKETMASTER_TOKEN;
       axios
-        .get(`discovery/v2/events.json?size=12&sort=random&countryCode=US&classificationName=Music&apikey=${apiKey}`)
+        .get(
+          `https://app.ticketmaster.com/discovery/v2/events.json?size=12&sort=random&countryCode=US&classificationName=Music&apikey=${apiKey}`
+        )
         .then((response) => {
           console.log("random events index", response);
           this.randomTickets = response.data._embedded.events;
@@ -360,7 +362,9 @@ export default {
     randomTicketAll: function () {
       let apiKey = process.env.VUE_APP_TICKETMASTER_TOKEN;
       axios
-        .get(`discovery/v2/events.json?size=100&sort=random&countryCode=US&classificationName=Music&apikey=${apiKey}`)
+        .get(
+          `https://app.ticketmaster.com/discovery/v2/events.json?size=100&sort=random&countryCode=US&classificationName=Music&apikey=${apiKey}`
+        )
         .then((response) => {
           console.log("random events all index", response);
           this.randomTickets = response.data._embedded.events;
